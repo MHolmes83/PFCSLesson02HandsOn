1eb6b5c32747832cecbf74dfcbd55703f551d65c
MHolmes83 <40703388+MHolmes83@users.noreply.github.com>
2023-11-15 09:58:21 -0500

    Lesson02HandsOn
    Created a simple dotnet project and ran an if...else statement that checked a customer's age and whether or not they were a student, then printed various strings to the console based on those two factors.

---
+++ Lesson02HandsOn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security;
+
+namespace Lesson02HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 3;
+            bool isStudent = true;
+
+            if(age >= 65)
+            {
+                Console.WriteLine("Please pay $7)");
+            }
+            else if(age < 0)
+            {
+                Console.WriteLine("Invalid age.");
+            }
+            else if(age <= 12 || isStudent)
+            {
+                Console.WriteLine("Please pay $8.");
+            }
+            else
+            {
+                Console.WriteLine("Please pay $10.");
+            }
+        }
+    }
+}
